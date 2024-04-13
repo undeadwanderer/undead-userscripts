929e466f2473db98aef521efa775af5953b26994
undeadwanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-04-13 17:06:50 +0300

    Metadata update

--- derpibooru-upload-tag-old.user.js
+++ derpibooru-upload-tag-old.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Derpibooru Default Rating Tag
+// @author       undead_wanderer
 // @description  Automatically adds the "safe" tag at the upload page. Made for personal use.
 // @version      0.6 | 2024-03-16
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
+// @license      CC BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/images/new$/
 // @include      https://ronxgr5zb4dkwdpt.onion/images/new
 // @require      https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js
