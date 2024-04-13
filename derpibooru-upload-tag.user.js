8b947cb862e3e1725e7b7be50d7d5042baa9ee83
undeadwanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-04-13 17:00:18 +0300

    Update derpibooru-upload-tag.user.js to v0.7.1
    derpibooru-upload-tag: Hotfix: Added a check to ensure the main part of the script runs on the upload page only.

--- derpibooru-upload-tag.user.js
+++ derpibooru-upload-tag.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Derpibooru Default Upload Tags
+// @author       undead_wanderer
 // @description  Automatically adds the "safe" tag at the upload page. Made for personal use.
-// @version      0.7 | 2024-04-13
+// @version      0.7.1 | 2024-04-13
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
+// @license      CC BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/(images/new|settings/edit)/
 // @include      /^https?://ronxgr5zb4dkwdpt\.onion/(images/new|settings/edit)/
 // @require      https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js
@@ -13,30 +15,33 @@
 // ==/UserScript==
 
 // configuration things
-var config = ConfigManager(
-    'Derpibooru Default Upload Tags',
-    'script_id',
-    'Automatically add tags to the upload page.'
+var config = ConfigManager (
+  'Derpibooru Default Upload Tags',
+  'script_id',
+  'Automatically add tags to the upload page.'
 );
-config.registerSetting({
-        title: 'Tags',
-        key: 'defaultTags',
-        description: 'Tags you want to see on the upload page by default. Separate with commas.',
-        type: 'text',
-        defaultValue: 'safe, pony'
-    })
+config.registerSetting ({
+  title: 'Tags',
+  key: 'defaultTags',
+  description: 'Tags you want to see on the upload page by default. Separate with commas.',
+  type: 'text',
+  defaultValue: 'safe, pony'
+});
 
-// fancy editor
-var Tags = config.getEntry('defaultTags');
+if (window.location.pathname === '/images/new') {
+  // fancy editor
+  var Tags = config.getEntry('defaultTags');
 
-var $fancyTags = $('.fancy-tag-upload');             // fancy tags div
-var $tagInput = $('input.js-taginput-fancy');        // tag input box
-var $switch1 = $('.js-taginput-show');               // fancy editor button
-var $switch2 = $('.js-taginput-hide');               // plain editor button
+  var $fancyTags = $('.fancy-tag-upload');             // fancy tags div
+  var $tagInput = $('input.js-taginput-fancy');        // tag input box
+  var $switch1 = $('.js-taginput-show');               // fancy editor button
+  var $switch2 = $('.js-taginput-hide');               // plain editor button
 
-if ($fancyTags.html().search('<span>') === -1) {    //check that there are no tags on page load
+  if ($fancyTags.html().search('<span>') === -1) {    //check that there are no tags on page load
+
     $switch2[0].click();
     var $normalTags = $('textarea.tagsinput');
     $normalTags[0].value += Tags;
     $switch1[0].click();
+  }
 }
