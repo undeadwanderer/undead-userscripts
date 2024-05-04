7bdf70456eeebc62965e7f2c6b15641b6aae38fa
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-04 20:02:06 +0300

    Create derpibooru-compact-image-names.user.js (version 1.0.0)
    Initial commit.

---
+++ derpibooru-compact-image-names.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name        Derpi compact image names
+// @author      undead_wanderer
+// @namespace   https://derpibooru.org/profiles/Pink%2BAmena
+// @include     /^https?://(www\.)?(derpi|trixie)booru\.org/.*$/
+// @include     https://ronxgr5zb4dkwdpt.onion/*
+// @grant       none
+// @version     1.0.0
+// @description New image links with less tags.
+// ==/UserScript==
+
+if (window.location.pathname.match(/(\/images)?\/\d+/i) !== null) {
+  const hostName = window.location.origin;
+  const imageId = window.location.pathname.replace(/(\/images)?\//i, '');
+  const imageFormat = document.querySelector('.image-size').innerHTML.match(/gif|jpg|jpeg|png|svg|webm/i)[0].toLowerCase();
+  let tagRatings = '';
+  let tagOrigin = '';
+  let tagCharacters = '';
+  const uploadDate = document.querySelector('time[datetime]').getAttribute('datetime').match(/\d\d\d\d-\d\d-\d\d/)[0].split(/-0?/);
+  const tagList = document.querySelectorAll('span[class="tag dropdown"]');
+
+  for (x of tagList) {
+    const escapeChars = {'-':'-dash-', '/':'-fwslash-', '\\':'-bwslash-', ':':'-colon-', '.':'-dot-', '+':'-plus-', ' ':'+'};
+    const tagId = x.dataset.tagName.replace(/-|\/|\\|\:|\.|\+| /g, function(match) {return escapeChars[match];});
+    if (x.dataset.tagCategory === 'origin') {
+      tagOrigin += '_' + tagId;
+    } else if (x.dataset.tagCategory === 'rating') {
+      tagRatings += '_' + tagId;
+    } else if (x.dataset.tagCategory === 'character' || x.dataset.tagCategory === 'oc') {
+      tagCharacters += '_' + tagId;
+    }
+  }
+
+  let imageName = imageId + '_' + tagRatings + tagOrigin + tagCharacters;
+  imageName = imageName.slice(0, 150) + '.' + imageFormat;
+  let viewLink = document.querySelector('a[title="View (tags in filename)"]');
+  let downloadLink = document.querySelector('a[title="Download (tags in filename)"]');
+  viewLink.href = 'https://derpicdn.net/img/view/' + uploadDate[0] + '/' + uploadDate[1] + '/' + uploadDate[2] + '/' + imageName;
+  downloadLink.href = 'https://derpicdn.net/img/download/' + uploadDate[0] + '/' + uploadDate[1] + '/' + uploadDate[2] + '/' + imageName;
+}
