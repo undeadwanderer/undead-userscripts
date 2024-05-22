acdd1fabfb44a24f6c4bd20b41a0254b760feb6c
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-22 18:12:16 +0300

    Update to v1.1 | Added escape words

--- derpibooru-forum-filter/derpibooru-forum-filter-derpi4u-legacy.user.js
+++ derpibooru-forum-filter/derpibooru-forum-filter-derpi4u-legacy.user.js
@@ -2,7 +2,7 @@
 // @name         Derpibooru Thread Filter Derpi4U legacy
 // @author       Undead_Wanderer
 // @description  An attempt to make the forums a little more worksafe by spoilering thread titles containing "NSFW". Made for personal use. May get false positives ("No NSFW"-type titles, etc.).
-// @version      1.00% / 2024-04-26
+// @version      1.1 / 2024-05-22
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      Creative Commons BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/.*$/
@@ -11,6 +11,8 @@
 // @inject-into  content
 // @grant        none
 // ==/UserScript==
+
+"use strict";
 
 // Derpi4U stuff goes here
 var config = ConfigManager(
@@ -25,15 +27,33 @@ config.registerSetting({
     type: 'text',
     defaultValue: 'nsfw, nfsw, not safe for work'
 });
+config.registerSetting({
+    title: 'Escape words',
+    key: 'escape0',
+    description: 'The words or phrases you wish to exclude from the filter. Case-insensitive. Separate with commas.',
+    type: 'text',
+    defaultValue: 'no nsfw'
+});
 
 // Main code
-var markerWords = config.getEntry('marker0').replace(/(\s*,\s*)/g,',').split(/,/); // Derpi4U-reliant variable for filter words list.
+const markerWords = config.getEntry('marker0').replace(/(\s*,\s*)/g,',').split(/,/); // Derpi4U-reliant variable for filter words list.
+const escapeWords = config.getEntry('escape0').replace(/(\s*,\s*)/g,',').split(/,/); // Derpi4U-reliant variable for filter words list.
 
 let x = document.querySelectorAll("a[href*='/forums/']");
-markerWords.forEach(function(item, index, arr){
+
+markerWords.forEach(function(item, index, arr) {
     for (let i of x) {
         if (i.innerHTML.toLowerCase().includes(arr[index].toLowerCase()) === true) {
-            i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+            escapeWords.forEach(function(item1, index1, arr1) {
+                if (i.innerHTML.toLowerCase().includes(arr1[index1].toLowerCase()) === true) {
+                     spoiler = false;
+                }
+            });
+			if (spoiler === true) {
+				i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+			} else {
+                spoiler = true;
+            }
         }
     }
 });
