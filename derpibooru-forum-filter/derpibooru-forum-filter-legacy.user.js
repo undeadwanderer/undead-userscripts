2ea73c08388f065171d36c52c86dc63ff2900fee
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-22 18:11:14 +0300

    Update to v1.1 | Added escape words

--- derpibooru-forum-filter/derpibooru-forum-filter-legacy.user.js
+++ derpibooru-forum-filter/derpibooru-forum-filter-legacy.user.js
@@ -2,7 +2,7 @@
 // @name         Derpibooru Thread Filter legacy
 // @author       Undead_Wanderer
 // @description  An attempt to make the forums a little more worksafe by spoilering thread titles containing "NSFW". Made for personal use. May get false positives ("No NSFW"-type titles, etc.).
-// @version      1.00* / 2024-04-26
+// @version      1.1 / 2024-05-22
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      Creative Commons BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/.*$/
@@ -11,14 +11,26 @@
 // @grant        none
 // ==/UserScript==
 
-const markerWords = ["NSFW", "NFSW", "Not safe for work"]; //filter words
+"use strict";
+
+const markerWords = ["NSFW", "NFSW", "Not safe for work"]; // filter words
+const escapeWords = ['No NSFW'] // exclusion words
 
 let x = document.querySelectorAll("a[href*='/forums/']");
 
-markerWords.forEach(function(item, index, arr){
+markerWords.forEach(function(item, index, arr) {
     for (let i of x) {
         if (i.innerHTML.toLowerCase().includes(arr[index].toLowerCase()) === true) {
-            i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+            escapeWords.forEach(function(item1, index1, arr1) {
+                if (i.innerHTML.toLowerCase().includes(arr1[index1].toLowerCase()) === true) {
+                     spoiler = false;
+                }
+            });
+			if (spoiler === true) {
+				i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+			} else {
+                spoiler = true;
+            }
         }
     }
 });
