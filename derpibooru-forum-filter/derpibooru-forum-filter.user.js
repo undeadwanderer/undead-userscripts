abd2120d1260b709aae1134a62f2322359a028dc
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-22 18:10:17 +0300

    Update to v1.1 | Added escape words

--- derpibooru-forum-filter/derpibooru-forum-filter.user.js
+++ derpibooru-forum-filter/derpibooru-forum-filter.user.js
@@ -2,7 +2,7 @@
 // @name         Derpibooru Thread Filter
 // @author       Undead_Wanderer
 // @description  An attempt to make the forums a little more worksafe by spoilering thread titles containing "NSFW". Made for personal use. May get false positives ("No NSFW"-type titles, etc.).
-// @version      1.00 / 2024-04-26
+// @version      1.1 / 2024-05-22
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      Creative Commons BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/.*$/
@@ -12,6 +12,8 @@
 // @grant        none
 
 // ==/UserScript==
+
+"use strict";
 
 // Derpi4U stuff goes here
 var config = ConfigManager(
@@ -26,17 +28,35 @@ config.registerSetting({
     type: 'textarea',
     defaultValue: 'nsfw\nnfsw\nnot safe for work'
 });
+config.registerSetting({
+    title: 'Escape words',
+    key: 'escape0',
+    description: 'The words or phrases you wish to exclude from the filter. Case-insensitive. Separate with newline.',
+    type: 'textarea',
+    defaultValue: 'no nsfw'
+});
 
 // Main code
 
 let markerWords = config.getEntry('marker0').replace(/(\s*\n\s*)/g,'\n').split(/\n+/); // Derpi4U-reliant variable for filter words list, multiline edition.
+let escapeWords = config.getEntry('escape0').replace(/(\s*\n\s*)/g,'\n').split(/\n+/); // Derpi4U-reliant variable for escape words list, multiline edition.
+let spoiler = true;
 
 let x = document.querySelectorAll("a[href*='/forums/']");
 
-markerWords.forEach(function(item, index, arr){
+markerWords.forEach(function(item, index, arr) {
     for (let i of x) {
         if (i.innerHTML.toLowerCase().includes(arr[index].toLowerCase()) === true) {
-            i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+            escapeWords.forEach(function(item1, index1, arr1) {
+                if (i.innerHTML.toLowerCase().includes(arr1[index1].toLowerCase()) === true) {
+                     spoiler = false;
+                }
+            });
+			if (spoiler === true) {
+				i.innerHTML = '<span class="spoiler">' + i.innerHTML + '</span>';
+			} else {
+                spoiler = true;
+            }
         }
     }
 });
