e44bbe5f928fe22b50cf3c39037e3f661ffec07c
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-27 12:51:08 +0300

    Update to v0.6.1; Tag field value check correction

--- derpibooru-upload-tag-legacy.user.js
+++ derpibooru-upload-tag-legacy.user.js
@@ -2,7 +2,7 @@
 // @name         Derpibooru Default Rating Tag Legacy Version
 // @author       undead_wanderer
 // @description  Automatically adds the "safe" tag at the upload page. Made for personal use.
-// @version      0.6 | 2024-03-16
+// @version      0.6.1 | 2024-05-27
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      CC BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/images/new$/
@@ -15,13 +15,13 @@
 // fancy editor
 
 var $fancyTags = $('.fancy-tag-upload');             // fancy tags div
+var $normalTags = $('textarea.tagsinput');
 var $tagInput = $('input.js-taginput-fancy');        // tag input box
 var $switch1 = $('.js-taginput-show');               // fancy editor button
 var $switch2 = $('.js-taginput-hide');               // plain editor button
 
-if ($fancyTags.html().search('<span>') === -1) {    //check that there are no tags on page load
+if ($normalTags.value === "") {    //check that there are no tags on page load
   $switch2[0].click();
-  var $normalTags = $('textarea.tagsinput');
   $normalTags[0].value += 'safe, ';
   $switch1[0].click();
 }
