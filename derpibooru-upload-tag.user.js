86d3a90b19a4196c2cdc905830dd63544b7c8a2b
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-05-27 12:50:55 +0300

    Update to v0.8.1; Tag field value check correction

--- derpibooru-upload-tag.user.js
+++ derpibooru-upload-tag.user.js
@@ -2,7 +2,7 @@
 // @name         Derpibooru upload page customization
 // @author       undead_wanderer
 // @description  Automatically adds tags and description of your choosing on the image upload page. Made for personal use.
-// @version      0.8.0 | 2024-05-17
+// @version      0.8.1 | 2024-05-27
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      CC BY-NC-SA 4.0
 // @include      /^https?://(www\.)?(derpi|trixie)booru\.org/(images/new|settings/edit)/
@@ -35,20 +35,21 @@ config.registerSetting ({
 });
 
 if (window.location.pathname === '/images/new') {
-    // fancy editor
-    let Tags = config.getEntry('defaultTags');
-    let Description = config.getEntry('defaultDesc');
 
-    let fancyTags = document.querySelector('.fancy-tag-upload');             // fancy tags div
-    let tagInput = document.querySelector('input.js-taginput-fancy');        // tag input box
-    let switch1 = document.querySelector('.js-taginput-show');               // fancy editor button
-    let switch2 = document.querySelector('.js-taginput-hide');               // plain editor button
+    const Tags = config.getEntry('defaultTags');
+    const Description = config.getEntry('defaultDesc');
+
+    const switch1 = document.querySelector('.js-taginput-show');               // fancy editor button
+    const switch2 = document.querySelector('.js-taginput-hide');               // plain editor button
+    // let fancyTags = document.querySelector('.fancy-tag-upload');             // fancy tags div
+    let normalTags = document.querySelector('textarea#image_tag_input'); // normal tags textarea
+    // let tagInput = document.querySelector('input.js-taginput-fancy');        // tag input box
     let descInput = document.querySelector('textarea#image_description');     // description
 
-    if (fancyTags.innerHTML.search('<span>') === -1) {    //check that there are no tags on page load
+    // if (fancyTags.innerHTML.search('<span>') === -1) {    //check that there are no tags on page load
 
+    if (normalTags.value === "") {    //check that there are no tags on page load
         switch2.click();
-        let normalTags = document.querySelector('textarea#image_tag_input');
         normalTags.value += Tags;
         switch1.click();
     }
