70fa5f840a1219c213e817a5dc413eea41b8452a
Undead Wanderer <51511863+undeadwanderer@users.noreply.github.com>
2024-06-12 17:23:35 +0300

    Update to 0.8.2 | Possibly fixed the new image page immediately proceeding to "uploading" on some occasions, did some code restructuring.

--- derpibooru-upload-tag-legacy.user.js
+++ derpibooru-upload-tag-legacy.user.js
@@ -1,27 +1,40 @@
 // ==UserScript==
-// @name         Derpibooru Default Rating Tag Legacy Version
+// @name         Derpibooru upload page customization
 // @author       undead_wanderer
-// @description  Automatically adds the "safe" tag at the upload page. Made for personal use.
-// @version      0.6.1 | 2024-05-27
+// @description  Automatically adds tags and description of your choosing on the image upload page. Made for personal use.
+// @version      0.8.2 | 2024-06-12
 // @namespace    https://derpibooru.org/profiles/Pink%2BAmena
 // @license      CC BY-NC-SA 4.0
-// @include      /^https?://(www\.)?(derpi|trixie)booru\.org/images/new$/
-// @include      https://ronxgr5zb4dkwdpt.onion/images/new
-// @require      https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.6/zepto.min.js
+// @include      /^https?://(www\.)?(derpi|trixie)booru\.org/(images/new|settings/edit)/
+// @include      /^https?://ronxgr5zb4dkwdpt\.onion/(images/new|settings/edit)/
 // @grant        none
 
 // ==/UserScript==
 
-// fancy editor
+"use strict";
 
-var $fancyTags = $('.fancy-tag-upload');             // fancy tags div
-var $normalTags = $('textarea.tagsinput');
-var $tagInput = $('input.js-taginput-fancy');        // tag input box
-var $switch1 = $('.js-taginput-show');               // fancy editor button
-var $switch2 = $('.js-taginput-hide');               // plain editor button
+// main code
+function main(){
+    // user variables
+    const Tags = 'safe';                                                      // tags you wish to see on the upload page by default. Separate with commas.
+    const Description = '';                                                   // text you wish to see in the description field on the upload page by default. "\n" for new line.
 
-if ($normalTags.value === "") {    //check that there are no tags on page load
-  $switch2[0].click();
-  $normalTags[0].value += 'safe, ';
-  $switch1[0].click();
+    // window element variables
+    const switch1 = document.querySelector('button.js-taginput-show');        // switch to fancy editor button
+    let normalTags = document.querySelector('textarea#image_tag_input');      // plaintext tags area
+    let descInput = document.querySelector('textarea#image_description');     // description field
+
+    if (normalTags.value === "") {                                            // check that there are no tags on page load
+        normalTags.value += Tags + ", ";
+        if (switch1.classList.contains('hidden') === true) {                  // check if fancy tags are enabled by default
+            switch1.click();                                                  // click the fancy editor buttton to apply the tags into the fancy view
+        }
+    }
+    if (descInput.value === "") {                                             // check that there is no description on page load
+        descInput.value += Description;
+    }
+};
+
+if (window.location.pathname === '/images/new') {
+    main();
 }
